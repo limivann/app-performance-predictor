df957835384caaba4cf206bdee4758cb63fe3ef2
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-02-14 15:12:22 +0800

    working scrapper for one appid only

--- google-play-scrapper/app.js
+++ google-play-scrapper/app.js
@@ -1,7 +1,50 @@
-var gplay = require("google-play-scraper");
+const gplay = require("google-play-scraper");
+const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+
+// csv headers
+const csvWriter = createCsvWriter({
+	path: "./output/output.csv",
+	header: [
+		{ id: "app_name", title: "APP_NAME" },
+		{ id: "rating", title: "RATING" },
+		{ id: "category", title: "CATEGORY" },
+		{ id: "rating_count", title: "RATING_COUNT" },
+		{ id: "installs", title: "INSTALLS" },
+		{ id: "min_installs", title: "MIN_INSTALLS" },
+		{ id: "max_installs", title: "MAX_INSTALLS" },
+		{ id: "free", title: "FREE" },
+		{ id: "price", title: "PRICE" },
+		{ id: "currency", title: "CURRENCY" },
+		{ id: "size", title: "SIZE" },
+		{ id: "content_rating", title: "CONTENT_RATING" },
+		{ id: "ad_supported", title: "AD_SUPPORTED" },
+		{ id: "in_app_purchases", title: "IN_APP_PURCHASES" },
+	],
+});
 
 gplay
 	.app({ appId: "com.google.android.apps.translate" })
-	.then(console.log, console.log);
-
-console.log("Scrapping complete!");
+	.then(async data => {
+		// array of objects
+		let formattedData = [
+			{
+				app_name: data.title,
+				rating: data.score,
+				category: data.genre,
+				rating_count: data.ratings,
+				installs: data.installs,
+				min_installs: data.minInstalls,
+				max_installs: data.maxInstalls,
+				free: data.free,
+				price: data.price,
+				currency: data.currency,
+				size: data.size,
+				content_rating: data.contentRating,
+				ad_supported: data.adSupported,
+				in_app_purchases: data.in_app_purchases,
+			},
+		];
+		await csvWriter.writeRecords(formattedData);
+	})
+	.then(() => console.log("Scrapped complete!"))
+	.catch(err => console.log("Something went wrong " + err));
