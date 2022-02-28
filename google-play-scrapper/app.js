829730cea6d67e8e464077eb15b502b936cb459b
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-02-28 14:45:05 +0800

    google play scrapper with more app details

--- google-play-scrapper/app.js
+++ google-play-scrapper/app.js
@@ -1,18 +1,32 @@
 // change this 1 to 500
-const numOfScrapes = 1;
+const numOfScrapes = 200;
+const fileName = "output_1";
 
 const gplay = require("google-play-scraper");
 const createCsvWriter = require("csv-writer").createObjectCsvWriter;
+const fs = require("fs");
 const categories = require("./constants/categories.js");
 const collections = require("./constants/collections");
 
+// create file
+fs.writeFile(`./output/${fileName}.csv`, "", err => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	//file written successfully
+});
+
 const csvWriter = createCsvWriter({
-	path: "./output/output.csv",
+	path: `./output/${fileName}.csv`,
 	header: [
 		{ id: "app_name", title: "APP_NAME" },
 		{ id: "rating", title: "RATING" },
 		{ id: "category", title: "CATEGORY" },
+		{ id: "collection", title: "COLLECTIONS" },
 		{ id: "rating_count", title: "RATING_COUNT" },
+		{ id: "rating_hist", title: "RATING_HISTOGRAM" },
+		{ id: "review_count", title: "REVIEW_COUNT" },
 		{ id: "installs", title: "INSTALLS" },
 		{ id: "min_installs", title: "MIN_INSTALLS" },
 		{ id: "max_installs", title: "MAX_INSTALLS" },
@@ -20,11 +34,14 @@ const csvWriter = createCsvWriter({
 		{ id: "price", title: "PRICE" },
 		{ id: "currency", title: "CURRENCY" },
 		{ id: "size", title: "SIZE" },
+		{ id: "developer", title: "DEVELOPER" },
 		{ id: "content_rating", title: "CONTENT_RATING" },
+		{ id: "family_genre", title: "FAMILY_GENRE" },
 		{ id: "ad_supported", title: "AD_SUPPORTED" },
 		{ id: "in_app_purchases", title: "IN_APP_PURCHASES" },
+		{ id: "released_date", title: "RELEASED_DATE" },
+		{ id: "last_updated", title: "LAST_UPDATED" },
 		{ id: "day_scraped", title: "DAY_SCRAPED" },
-		{ id: "collection", title: "COLLECTIONS" },
 	],
 });
 const numOfCats = categories.length;
@@ -46,17 +63,25 @@ const formatDate = (date, format) => {
 	return format.replace(/mm|dd|yy|yyy/gi, matched => map[matched]);
 };
 
+const sleepFor = sleepDuration => {
+	const now = new Date().getTime();
+	while (new Date().getTime() < now + sleepDuration) {
+		/* Do nothing */
+	}
+};
+
 console.log("Scrapping all Cats ... ");
 console.time(
 	`Time for scraping ${numOfScrapes} for each ${numOfCats} categories`
 );
 
-const scrappedAllDataPromise = new Promise((resolveSAP, rej) => {
+const scrappedAllDataPromise = new Promise((resolveSAP, rejSAP) => {
 	collections.forEach(async collection => {
+		sleepFor(2000);
 		console.log(`SCRAPING COLLECTION: ${collection}`);
 		console.log("==================================");
 		categories.forEach(async category => {
-			const categoryPromise = new Promise((resCP, rej) => {
+			const categoryPromise = new Promise((resCP, rejCP) => {
 				gplay
 					.list({
 						category: category,
@@ -70,11 +95,19 @@ const scrappedAllDataPromise = new Promise((resolveSAP, rej) => {
 						const formattedDate = formatDate(today, "dd/mm/yy");
 
 						data.map(async appDetails => {
+							const dateObj = new Date(appDetails.updated);
+							const lastUpdatedDate = formatDate(dateObj, "dd/mm/yy");
+
+							const rating_hist = JSON.stringify(appDetails.histogram);
+
 							formattedData.push({
 								app_name: appDetails.title,
 								rating: appDetails.score,
 								category: appDetails.genre,
+								collection: collection,
 								rating_count: appDetails.ratings,
+								rating_hist: rating_hist,
+								review_count: appDetails.reviews,
 								installs: appDetails.installs,
 								min_installs: appDetails.minInstalls,
 								max_installs: appDetails.maxInstalls,
@@ -82,11 +115,14 @@ const scrappedAllDataPromise = new Promise((resolveSAP, rej) => {
 								price: appDetails.price,
 								currency: appDetails.currency,
 								size: appDetails.size,
+								developer: appDetails.developer,
 								content_rating: appDetails.contentRating,
+								family_genre: appDetails.familyGenre,
 								ad_supported: appDetails.adSupported,
 								in_app_purchases: appDetails.offersIAP,
+								released_date: appDetails.released,
+								last_updated: lastUpdatedDate,
 								day_scraped: formattedDate,
-								collection: collection,
 							});
 						});
 						resCP(formattedData);
@@ -94,14 +130,25 @@ const scrappedAllDataPromise = new Promise((resolveSAP, rej) => {
 						console.log(
 							`SCRAPING COMPLETE FOR ${category} FOR THE COLLECTION ${collection}`
 						);
+					})
+					.catch(err => {
+						console.log("ERROR: " + err);
+						rejCP(err);
+						currentCount++;
 					});
 			});
-			categoryPromise.then(async data => {
-				finalCsvData = [...finalCsvData, ...data];
-				if (currentCount == countGoal) {
-					resolveSAP(finalCsvData);
-				}
-			});
+			categoryPromise
+				.then(async data => {
+					finalCsvData = [...finalCsvData, ...data];
+					if (currentCount == countGoal) {
+						resolveSAP(finalCsvData);
+					}
+				})
+				.catch(err => {
+					if (currentCount == countGoal) {
+						resolveSAP(finalCsvData);
+					}
+				});
 		});
 	});
 });
--- google-play-scrapper/constants/collections.js
+++ google-play-scrapper/constants/collections.js
@@ -3,6 +3,14 @@ const collections = [
 	gplay.collection.TOP_FREE,
 	gplay.collection.TOP_PAID,
 	gplay.collection.GROSSING,
+	// gplay.collection.TOP_FREE_GAMES,
+	// gplay.collection.TOP_PAID_GAMES,
+	// gplay.collection.TOP_GROSSING_GAMES,
+	// gplay.collection.TRENDING,
+	// gplay.collection.NEW_FREE,
+	// gplay.collection.NEW_PAID,
+	// gplay.collection.NEW_FREE_GAMES,
+	// gplay.collection.NEW_PAID_GAMES,
 ];
 
 module.exports = collections;
