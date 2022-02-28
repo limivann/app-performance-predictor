95b863ed252c9b0dad425632bf31c2feac2f3ad1
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-02-28 21:50:45 +0800

    streamlit deploy test

--- streamlit-app/main.py
+++ streamlit-app/main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write(
 """
-    # App rating predictor      
-    Simple app rating predictor using streamlit
+    # App rating predictor     
+    Simple app rating predictor for SC1015 Project
 """)
 
