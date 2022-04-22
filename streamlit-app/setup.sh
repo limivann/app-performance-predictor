dd368c6cdea246e1457f8982ee348cbac841b956
LimIvan336 <71662324+LimIvan336@users.noreply.github.com>
2022-04-23 02:17:08 +0800

    setup config

--- streamlit-app/setup.sh
+++ streamlit-app/setup.sh
@@ -1,9 +1,15 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]\n\
+primaryColor = '#648fd1'\n\
+backgroundColor = '#f0f0f5'\n\
+secondaryBackgroundColor = '#e0e0ef'\n\
+textColor= '#262730'\n\
+font = 'sans serif'\n\
 [server]\n\
+headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
