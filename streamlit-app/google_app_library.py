cb2b4c8be14873c27048964fc60ac5396100ddbf
Ivan Lim <71662324+limivann@users.noreply.github.com>
2022-04-24 01:13:33 +0800

    Delete google_app_library.py

--- streamlit-app/google_app_library.py
+++
@@ -1,8 +0,0 @@
-# importing google play scrapper
-from google_play_scraper import app
-
-result = app(
-    'com.nianticlabs.pokemongo',
-    lang='en', # defaults to 'en'
-    country='us' # defaults to 'us'
-)
